4c195fa3131e4675b442d508d8433d81d23a9648
Dmytro Sokil <dmytro.sokil@gmail.com>
2016-09-29 23:51:16 +0300

    empty block end axtended i18n

--- src/Resources/assets/components/app/i18n.js
+++ src/Resources/assets/components/app/i18n.js
@@ -1,12 +1,60 @@
-/**
- * Localization
- */
 var i18n = {
-    _messages: {},
-    addMessages: function(messages) {
-        this._messages = $.extend({}, this._messages, messages);
+
+    /**
+     * List of messages
+     */
+    messages: {},
+
+    /**
+     * Reset message list
+     */
+    reset: function() {
+        this.messages = {};
+        return this;
     },
-    getMessage: function(code) {
-        return this._messages[code] || code;
+
+    /**
+     * Add new message
+     * @param {string} key
+     * @param {string} value
+     */
+    setMessage: function(key, value) {
+        this.messages[key] = value;
+        return this;
+    },
+
+    /**
+     * Append list of messages
+     * @param messages
+     */
+    addMessages: function(messages) {
+        for(var key in messages) {
+            this.messages[key] = messages[key];
+        }
+        return this;
+    },
+
+    /**
+     * Get message by key
+     *
+     * @param {string} key
+     * @param {string} placeholders
+     * @returns {string}
+     */
+    getMessage: function(key, placeholders) {
+        // get message
+        var message = this.messages[key] || key;
+
+        // replace placeholders
+        if (placeholders) {
+            for(var placeholder in placeholders) {
+                message = message.replace(
+                    new RegExp(placeholder, 'g'),
+                    placeholders[placeholder]
+                );
+            };
+        }
+
+        return message;
     }
 };
