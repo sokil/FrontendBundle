bb98dd77ac5073e5a123e3c06470c5ad360d5e2c
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-01-31 00:53:41 +0200

    change ext name

--- src/DependencyInjection/FrontendExtension.php
+++ src/DependencyInjection/FrontendExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class SpaExtension extends Extension
+class FrontendExtension extends Extension
 {
     /**
      * {@inheritdoc}
